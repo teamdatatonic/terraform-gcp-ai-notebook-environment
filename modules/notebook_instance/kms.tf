f7de81dc9a935ba5bb1ba308bd5336ae82bbc608
simonbutt-datatonic <simon.butt@datatonic.com>
2021-04-06 14:18:30 +0100

    fix(kms): fixing checkov by removing option for key rotation lookup and instead forcing 30 days
    Signed-off-by: simonbutt-datatonic <simon.butt@datatonic.com>

--- modules/notebook_instance/kms.tf
+++ modules/notebook_instance/kms.tf
@@ -11,6 +11,6 @@ resource "google_kms_crypto_key" "instance_key" {
   name     = "${each.key}-nb-instance-key"
   key_ring = google_kms_key_ring.notebook_keyring.id
 
-  rotation_period = lookup(each.value, "kms_rotation_period", "100000s")
+  rotation_period = "2592000s"
 
 }
